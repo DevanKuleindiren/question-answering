10c7187dc4d8501080dcb53b5e0ceaff06cd84a3
DevanKuleindiren <d.kuleindiren@gmail.com>
2019-03-16 00:40:59 +0000

    Add methods to score documents using weighted bag-of-words cosine similarity.

--- ngram_aggregator.py
+++ ngram_aggregator.py
@@ -8,12 +8,12 @@ class NgramAggregator:
         self.document_magnitudes = {}
         self.doc_frequency = {}
 
-    def aggregate(self, n_grams, shouldAggregate=True):
+    def aggregate(self, n_grams, aggregate_doc_frequency=True):
         s = {}
         for n_gram in n_grams:
             key = hash(n_gram)
             s[key] = s.get(key, 0) + 1
-            if s[key] == 1 and shouldAggregate:
+            if s[key] == 1 and aggregate_doc_frequency:
                 self.doc_frequency[key] = 1 + self.doc_frequency.get(key, 0)
         return s
 
@@ -23,7 +23,8 @@ class NgramAggregator:
             ls = self.ngram_to_docs.get(hashedNgram, [])
             ls.append((doc_id, tf))
             self.ngram_to_docs[hashedNgram] = ls
-            tf_idf = tf / self.doc_frequency[hashedNgram]
+            # Add 1 to the document frequency to avoid division by 0.
+            tf_idf = tf / (self.doc_frequency[hashedNgram] + 1)
             magnitude += tf_idf * tf_idf
         self.document_magnitudes[doc_id] = sqrt(magnitude)
 
@@ -34,3 +35,24 @@ class NgramAggregator:
         if doc_id in self.document_magnitudes:
             return self.document_magnitudes[doc_id]
         return 0
+
+    def merge_results(self, query_ngram_to_docs, query_magnitude):
+        scores = {}
+        for (query_ngram, docs) in query_ngram_to_docs:
+            query_ngram_df = self.doc_frequency[query_ngram]
+            for (doc, tf) in docs:
+                # Add 1 to the document frequency to avoid division by 0.
+                scores[doc] = scores.get(doc, 0) + tf / (query_ngram_df + 1)
+        for doc in scores:
+            scores[doc] /= self.document_magnitudes[doc] * query_magnitude
+        return scores
+
+    def query(self, query_ngrams):
+        # Filter out duplicates
+        query_ngrams = self.aggregate(query_ngrams, aggregate_doc_frequency=False).keys()
+        query_magnitude = sqrt(len(query_ngrams))
+        query_ngram_to_docs = []
+        for query_ngram in query_ngrams:
+            if query_ngram in self.ngram_to_docs:
+                query_ngram_to_docs.append((query_ngram, self.ngram_to_docs[query_ngram]))
+        return self.merge_results(query_ngram_to_docs, query_magnitude)
--- ngram_aggregator_test.py
+++ ngram_aggregator_test.py
@@ -13,13 +13,14 @@ class TestNgramAggregator(unittest.TestCase):
         self.ngram2 = Ngram(["again", "another"])
         self.ngram3 = Ngram(["world", "hello"])
         self.doc1 = [self.ngram1, self.ngram2, self.ngram1]
-        self.doc2 = [self.ngram1, self.ngram2, self.ngram2, self.ngram3]
+        self.doc2 = [self.ngram1, self.ngram2, self.ngram2, self.ngram3, self.ngram2]
+        self.query = [self.ngram1, self.ngram2]
         self.s1 = self.agg.aggregate(self.doc1)
         self.s2 = self.agg.aggregate(self.doc2)
 
     def test_aggregate(self):
         self.assertDictEqual(self.s1, {hash(self.ngram1): 2, hash(self.ngram2): 1})
-        self.assertDictEqual(self.s2, {hash(self.ngram1): 1, hash(self.ngram2): 2, hash(self.ngram3): 1})
+        self.assertDictEqual(self.s2, {hash(self.ngram1): 1, hash(self.ngram2): 3, hash(self.ngram3): 1})
 
     def test_add_to_representation(self):
         doc1_id = "doc1"
@@ -30,11 +31,41 @@ class TestNgramAggregator(unittest.TestCase):
 
         self.agg.add_to_representation(doc2_id, self.s2)
         self.assertDictEqual(self.agg.get_representation(), {hash(self.ngram1): [(doc1_id, 2), (doc2_id, 1)],
-                                                             hash(self.ngram2): [(doc1_id, 1), (doc2_id, 2)],
+                                                             hash(self.ngram2): [(doc1_id, 1), (doc2_id, 3)],
                                                              hash(self.ngram3): [(doc2_id, 1)]})
 
     def test_get_magnitude(self):
         doc1_id = "doc1"
         self.agg.add_to_representation(doc1_id, self.s1)
         self.assertEqual(self.agg.get_magnitude("blah"), 0)
-        self.assertEqual(self.agg.get_magnitude(doc1_id), sqrt(2 * 2 / (2 * 2) + 1 * 1 / (2 * 2)))
+        # |d1| = √((2/3)^2 + (1/3)^2) = √5 / 3
+        self.assertAlmostEqual(self.agg.get_magnitude(doc1_id), sqrt(5) / 3, places=5)
+
+    def test_single_doc_query(self):
+        doc1_id = "doc1"
+        self.agg.add_to_representation(doc1_id, self.s1)
+        # |q| = √2, |d1| = √5 / 3
+        # Therefore, cosine_similarity(q, d1) = (2/3 + 1/3) / (|q| * |d1|)
+        #                                     = 1 / (√2 * √5/3)
+        #                                     = 3 / √10
+        query_result = self.agg.query(self.query)
+        self.assertEqual(len(query_result), 1)
+        self.assertTrue(doc1_id in query_result)
+        self.assertAlmostEqual(query_result[doc1_id], 3 / sqrt(10))
+
+    def test_multiple_doc_query(self):
+        doc1_id = "doc1"
+        doc2_id = "doc2"
+        self.agg.add_to_representation(doc1_id, self.s1)
+        self.agg.add_to_representation(doc2_id, self.s2)
+        # d1 score is same as above.
+        # |q| = √2, |d2| = √((1/3)^2 + (3/3)^2 + (1/2)^2) = √(49/36) = 7/6
+        # Therefore, cosine_similarity(q, d2) = (1/3 + 3/3) / (|q| * |d2|)
+        #                                     = 4/3 / (√2 * 7/6)
+        #                                     = 8 / (7 * √2)
+        query_result = self.agg.query(self.query)
+        self.assertEqual(len(query_result), 2)
+        self.assertTrue(doc1_id in query_result)
+        self.assertTrue(doc2_id in query_result)
+        self.assertAlmostEqual(query_result[doc1_id], 3 / sqrt(10))
+        self.assertAlmostEqual(query_result[doc2_id], 8 / (7 * sqrt(2)))
