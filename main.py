2fd6448991c40f4678576ee867742703d166b316
DevanKuleindiren <d.kuleindiren@gmail.com>
2019-03-20 12:14:19 +0000

    Add support to main.py for querying over a list of JSON documents from e.g. a Wikipedia dump.

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
+import argparse
+import json
+import string
+
 from indexing.ngram_generator import NgramGenerator
 from indexing.ngram_postings_list import NgramPostingsList
 
-doc1 = "hello world hello world"
-doc2 = "hello world hello again"
-query = "hello world"
+flag_parser = argparse.ArgumentParser(description="Send queries to a specified list of documents.")
+flag_parser.add_argument(
+    '--docs_file_path', help='File path to the JSON file containing documents.', dest='docs_file_path')
 
-bigram_generator = NgramGenerator(2)
-bigram_postings_list = NgramPostingsList()
-bigram_postings_list.add_to_representation("doc1", bigram_postings_list.aggregate(bigram_generator.generate_ngrams(doc1)))
-bigram_postings_list.add_to_representation("doc2", bigram_postings_list.aggregate(bigram_generator.generate_ngrams(doc2)))
 
-print(bigram_postings_list.query(bigram_generator.generate_ngrams(query)))
+def main():
+    flags = flag_parser.parse_args()
+
+    if flags.docs_file_path is None:
+        print('--docs_file_path needs to be set.')
+        return
+
+    bigram_generator = NgramGenerator(2)
+    bigram_postings_list = NgramPostingsList()
+    doc_index = {}
+
+    with open(flags.docs_file_path) as docs_file:
+        for doc_json in docs_file:
+            json_data = json.loads(doc_json)
+            doc_url = json_data['url']
+            doc_id = hash(doc_url)
+            doc_index[doc_id] = doc_url
+            doc_text = json_data['text'].translate(str.maketrans('', '', string.punctuation))
+            bigram_postings_list.add_to_representation(
+                doc_id, bigram_postings_list.aggregate(bigram_generator.generate_ngrams(doc_text)))
+    print('Indexed %d docs' % len(doc_index))
+
+    while True:
+        query_text = input('Query: ')
+        query_bigrams = bigram_generator.generate_ngrams(query_text)
+        top_documents = bigram_postings_list.query(query_bigrams)
+        sorted_top_documents = sorted(top_documents.items(), key=lambda kv: kv[1], reverse=True)
+        sorted_top_documents_with_urls = [(doc_id, doc_index[doc_id], score)
+                                          for (doc_id, score) in sorted_top_documents]
+        print(sorted_top_documents_with_urls)
+
+
+if __name__ == '__main__':
+    main()
