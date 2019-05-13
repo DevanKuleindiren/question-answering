e576ee21a2a7d3bf587307854c688bf8238d0456
dim131 <dim-los_13@hotmail.com>
2019-05-14 00:58:58 +0100

    Update NLTK feature generator to provide the lemmatizer in its new format

--- chen_et_al/nltk_feature_generator.py
+++ chen_et_al/nltk_feature_generator.py
@@ -7,9 +7,10 @@ from nltk.chunk import tree2conlltags
 
 def create_nltk_feature_generator(word_embedding_vector_generator, tf_supplier):
     # nltk.download()
+    lemmatizer = PorterStemmer()
     return FeatureVectorGenerator(
         word_embedding_generator=word_embedding_vector_generator,
-        lemmatizer=PorterStemmer(),# TODO: Find out why word net lemmatization does not work.
+        lemmatizer=lambda x: lemmatizer.stem(x),# TODO: Find out why word net lemmatization does not work.
         pos_tagger=lambda x: [tag for (_, tag) in pos_tag(x)],
         ner_tagger=lambda x: [x.startswith("B-") for (_, _, x) in tree2conlltags(ne_chunk(pos_tag(x)))],
         tf_supplier=tf_supplier
